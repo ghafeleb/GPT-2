e3514b340c2517b0499663b2e9c6af6965e7b6c6
Ali Ghafelebashi <59674668+ghafeleb@users.noreply.github.com>
2024-07-09 15:49:03 -0700

    Computed first training loss

--- model/gpt2.py
+++ model/gpt2.py
@@ -114,7 +114,11 @@ class GPT(nn.Module):
         x = self.transformer.ln_f(x)
         # Classifier
         logits = self.lm_head(x) # Shape: (B, T, vocab_size)
-        return logits
+        
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
 
 
     @classmethod
